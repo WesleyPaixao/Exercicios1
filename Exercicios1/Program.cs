516c988e18fd870559011decf0efc2c47a62405f
Wesley Paixão <wesleygp13@hotmail.com>
2022-02-28 10:29:41 -0300

    Exercicio 1

--- Exercicios1/Program.cs
+++ Exercicios1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Exercicios1
 {
@@ -6,7 +7,34 @@ namespace Exercicios1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*Fazer um programa para ler o código de uma peça 1, o número de peças 1, o valor unitário de cada peça 1, o 
+             código de uma peça 2, o número de peças 2 e o valor unitário de cada peça 2. Calcule e mostre o valor a ser pago.
+            */
+
+            int cd1, n1,cd2, n2;
+            double v1, v2;
+
+            Console.Write("Informe o codigo da peça: ");
+            cd1 = int.Parse(Console.ReadLine());
+            Console.Write("Informe o numero de peças: ");
+            n1 = int.Parse(Console.ReadLine());
+            Console.Write("Informe o valor unitario de cada peça: ");
+            v1 = double.Parse(Console.ReadLine());
+
+            var T1 = n1 * v1;
+
+            Console.Write("Informe o codigo da segunda peça: ");
+            cd2 = int.Parse(Console.ReadLine());
+            Console.Write("Informe o numero de peças: ");
+            n2 = int.Parse(Console.ReadLine());
+            Console.Write("Informe o valor unitario de cada peça ");
+            v2 = double.Parse(Console.ReadLine()); 
+
+            var T2 = n2 * v2;
+
+            var Total = T1 + T2;
+            Console.Write("TOTAL A PAGAR R$: "+Total.ToString("F2",CultureInfo.InvariantCulture));
+
         }
     }
 }
