3a396bf226f06437bb99c0863cb340306bdc5265
Wesley Paixão <wesleygp13@hotmail.com>
2022-02-28 10:08:16 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicios1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercicios1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
